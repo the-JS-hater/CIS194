331befdec0e14b4184e172da01134a4dbe3ebdec
Morgan <morno368@student.liu.se>
2024-10-06 13:48:14 +0200

    card number validation excercise done

--- homework1.hs
+++ homework1.hs
@@ -15,3 +15,18 @@ toDigitsRev n
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther [] = []
 doubleEveryOther (x : xs : xss) = [x, xs * 2] ++ (doubleEveryOther xss)
+
+--Exercise 3 
+
+sumDigits :: [Integer] -> Integer
+sumDigits nums = sum(map (\n -> (n `div` 10) + (n `mod` 10)) nums)
+
+--Exercise 4 
+
+validate :: Integer -> Bool
+validate cardNum = sumDigits (doubleEveryOther (toDigitsRev cardNum)) `mod` 10 == 0
+
+--Example: validate 4012888888881881 = True
+--Example: validate 4012888888881882 = False
+
+
